eac8f9dcb255591e4c697f5e5ba2cbde9c8c11ba
Kooooooksz <147242464+Kooooooksz@users.noreply.github.com>
2024-07-10 16:59:44 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, request, render_template, redirect, url_for
+import pandas as pd
+import os
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+
+# Create the upload folder if it doesn't exist
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        return redirect(request.url)
+    if file and file.filename.endswith('.xlsx'):
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(file_path)
+        return redirect(url_for('calculate_average', filename=file.filename))
+    return redirect(request.url)
+
+
+@app.route('/calculate/<filename>')
+def calculate_average(filename):
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    df = pd.read_excel(file_path)
+
+    kreditek = list(df["Kr."])
+    jegyek = list(df["Jegyek"])
+
+    def utsozarpojel(szo):
+        index = 0
+        for i in range(len(szo)):
+            if szo[i] == "(":
+                index = i
+        return index
+
+    szorzat = 0
+    hagyomanyos, oszto  = 0, 0
+    osszkredit = sum(kreditek)
+    teljesitettkredit = 0
+    for i in range(len(kreditek)):
+        if kreditek[i] != 0 and int(jegyek[i][utsozarpojel(jegyek[i]) + 1]) != 1:
+            szorzat += kreditek[i] * int(jegyek[i][utsozarpojel(jegyek[i]) + 1])
+            hagyomanyos += int(jegyek[i][utsozarpojel(jegyek[i]) + 1])
+            oszto += 1
+            teljesitettkredit += kreditek[i]
+
+    average = szorzat / 30 * (teljesitettkredit/osszkredit)
+    sulyozottatlag = szorzat/osszkredit
+    return render_template('result.html', kki=average, traditional_average=hagyomanyos/oszto, teljesitettkredit=teljesitettkredit, osszkredit=osszkredit, sulyozottatlag=sulyozottatlag)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
